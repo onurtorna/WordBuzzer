234e6b418e479cb46f90b7f31712cecd9c89f2b9
Onur Torna <onurtorna@gmail.com>
2018-04-25 15:58:22 +0300

    Add global constant file

---
+++ WordBuzzer/WordBuzzer/Global.swift
@@ -0,0 +1,21 @@
+//
+//  Global.swift
+//  WordBuzzer
+//
+//  Created by Onur Torna on 25/04/18.
+//  Copyright © 2018 Onur Torna. All rights reserved.
+//
+
+import Foundation
+
+enum Global {
+
+    enum File {
+        static let jsonFolderPath = "words"
+
+        enum Extension {
+            static let json = "json"
+        }
+    }
+
+}
