bcebc6d348d65cc4a1e5e347c59a815d0daefa2e
Onur Torna <onurtorna@gmail.com>
2018-04-26 10:54:22 +0300

    Add nib loadable protocol

---
+++ WordBuzzer/WordBuzzer/Protocols/NibLoadable.swift
@@ -0,0 +1,37 @@
+//
+//  NibLoadable.swift
+//  WordBuzzer
+//
+//  Created by Onur Torna on 26/04/18.
+//  Copyright © 2018 Onur Torna. All rights reserved.
+//
+
+import UIKit
+
+protocol NibLoadable {
+    static var defaultNibName: String { get }
+    static func loadFromNib() -> Self
+}
+
+extension NibLoadable where Self: UIView {
+
+    static var defaultNibName: String {
+        return String(describing: self)
+    }
+
+    static var defaultNib: UINib {
+        return UINib(nibName: defaultNibName, bundle: nil)
+    }
+
+    static func loadFromNib() -> Self {
+        guard let nib = Bundle.main.loadNibNamed(defaultNibName,
+                                                 owner: nil,
+                                                 options: nil)?.first as? Self
+            else {
+                fatalError("[NibLoadable] Cannot load view from nib.")
+        }
+
+        return nib
+    }
+
+}
