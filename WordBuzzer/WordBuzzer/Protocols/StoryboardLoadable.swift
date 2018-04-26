4b8f074197680a80c4485d5f2b9fd745560211bb
Onur Torna <onurtorna@gmail.com>
2018-04-26 15:26:39 +0300

    Add storyboard loadable protocol

--- WordBuzzer/WordBuzzer/Protocols/StoryboardLoadable.swift
+++ WordBuzzer/WordBuzzer/Protocols/StoryboardLoadable.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2018 Onur Torna. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol StoryboardLoadable {
+    static var defaultStoryboardName: String { get }
+    static var defaultViewControllerID: String { get }
+    static func loadFromStoryboard() -> Self
+}
+
+extension StoryboardLoadable where Self: UIViewController {
+
+    static var defaultViewControllerID: String {
+        return String(describing: self)
+    }
+
+    static func loadFromStoryboard() -> Self {
+        let storyboard = UIStoryboard(name: defaultStoryboardName, bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: defaultViewControllerID) as? Self else {
+            fatalError("[StoryboardLoadable] Cannot instantiate view controller from storyboard.")
+        }
+        return viewController
+    }
+}
