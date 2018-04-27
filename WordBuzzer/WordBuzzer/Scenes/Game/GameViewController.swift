8451eea33572a7b7343c5a8648e53daa49da212f
Onur Torna <onurtorna@gmail.com>
2018-04-27 17:02:12 +0300

    Add UI Components and actions for game screen

--- WordBuzzer/WordBuzzer/Scenes/Game/GameViewController.swift
+++ WordBuzzer/WordBuzzer/Scenes/Game/GameViewController.swift
@@ -13,7 +13,61 @@ class GameViewController: UIViewController, StoryboardLoadable {
     /// Default storyboard name to conform to StoryboardLoadable protocol
     static let defaultStoryboardName = "Main"
 
+    @IBOutlet private weak var playerOneScoreLabel: UILabel!
+    @IBOutlet private weak var playerTwoScoreLabel: UILabel!
+    @IBOutlet private weak var playerThreeScoreLabel: UILabel!
+    @IBOutlet private weak var playerFourScoreLabel: UILabel!
+    @IBOutlet private weak var askedWordLabel: UILabel!
+    @IBOutlet private weak var remainingRoundLabel: UILabel!
+    
+    @IBOutlet private weak var playerOneBuzzerButton: UIButton!
+    @IBOutlet private weak var playerTwoBuzzerButton: UIButton!
+    @IBOutlet private weak var playerThreeBuzzerButton: UIButton!
+    @IBOutlet private weak var playerFourBuzzerButton: UIButton!
+    @IBOutlet private weak var pauseButton: UIButton!
+
+    @IBOutlet private weak var wordContainerView: UIView!
+
     /// View model
     var viewModel: GameViewModel!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewModel.stateChangeHandler = { change in
+            self.applyState(change: change)
+        }
+
+        configureViews()
+    }
+
+    func applyState(change: GameState.Change) {
+        // TODO: To be implemented
+        switch change {
+        case .gameStarted:
+            break
+        case .gameActivateStateChanged(let isGameActive):
+            break
+        }
+    }
+
+    private func configureViews() {
+        // TODO: To be implemented
+    }
+
+}
+
+// MARK: - Actions
+extension GameViewController {
+    // TODO: To be implemented
+    @IBAction private func playerOneBuzzerButtonTapped(_ sender: Any) {
+    }
+
+    @IBAction private func playerTwoBuzzerButtonTapped(_ sender: Any) {
+    }
+    @IBAction private func playerThreeBuzzerButtonTapped(_ sender: Any) {
+    }
+
+    @IBAction private func playerFourBuzzerButtonTapped(_ sender: Any) {
+    }
 }
